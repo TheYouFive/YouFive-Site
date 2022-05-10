b2157647b0e4fd7a7997141429d869ecd6dd10ee
YouFive <dbxor1568@naver.com>
2022-05-10 19:06:31 +0900

    semi-finish

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,16 +1,17 @@
 const backTo = document.querySelector("#backtotop");
 
 function scrolldown() {
-  let pageY = window.pageYOffset;
-  if (pageY !== 0) {
-    backTo.classList.add("show");
+  let pageY = window.scrollY;
+  if (pageY > 0) {
+    backTo.classList.add("btn-appear");
   } else {
-    backTo.classList.remove("show");
+    backTo.classList.remove("btn-appear");
   }
 }
 
 function clickTop() {
-  if (window.pageYOffset > 0) {
+  let pageY = window.scrollY;
+  if (pageY > 0) {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
